55b74484b758e5780e87a8a13ef6e78e7cdffded
Nayoung Kim <nka50@sfu.ca>
2025-10-21 18:30:48 -0700

    Add DashboardBox component with box styling

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,6 +1,7 @@
-import { Box, useTheme } from "@mui/material";
+import DashboardBox from "@/components/DashboardBox";
+import { Box, useMediaQuery } from "@mui/material";
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
 "a b c"
 "a b c"
 "a b c"
@@ -12,20 +13,74 @@ const gridTemplate = `
 "g h j"
 "g h j"
 `;
+
+const gridTemplateSmallScreens = `
+"a"
+"a"
+"a"
+"a"
+"b"
+"b"
+"b"
+"b"
+"c"
+"c"
+"c"
+"d"
+"d"
+"d"
+"e"
+"e"
+"f"
+"f"
+"f"
+"g"
+"g"
+"g"
+"h"
+"h"
+"h"
+"h"
+"i"
+"i"
+"j"
+"j"
+`;
+
 const Dashboard = () => {
-  const { palette } = useTheme();
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+
   return (
     <Box
       width="100%"
       height="100%"
       display="grid"
       gap="1.5rem"
-      sx={{
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-        gridTemplateAreas: gridTemplate,
-      }}
-    ></Box>
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <DashboardBox gridArea="a"></DashboardBox>
+      <DashboardBox gridArea="b"></DashboardBox>
+      <DashboardBox gridArea="c"></DashboardBox>
+      <DashboardBox gridArea="d"></DashboardBox>
+      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="f"></DashboardBox>
+      <DashboardBox gridArea="g"></DashboardBox>
+      <DashboardBox gridArea="h"></DashboardBox>
+      <DashboardBox gridArea="i"></DashboardBox>
+      <DashboardBox gridArea="j"></DashboardBox>
+    </Box>
   );
 };
 
