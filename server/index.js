2978b8fa88f335e1b22dcc2f072f582fe3ed8309
Nayoung Kim <nka50@sfu.ca>
2025-10-23 16:45:02 -0700

    add server entry point with middleware, routes, MongoDB connection, and mock data seeding

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -25,7 +27,11 @@ app.use("/kpi", kpiRoutes);
 const PORT = process.env.PORT || 9000;
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => {
+  .then(async () => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
   })
   .catch((error) => console.log(`MongoDB connections failed: ${error}`));
