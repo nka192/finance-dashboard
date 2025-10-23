ab7c30b7bfac53bf5a2e898cb3888bb2ede37f2c
Nayoung Kim <nka50@sfu.ca>
2025-10-22 20:26:38 -0700

    add MongoDB connection and server startup logic

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,8 +1,8 @@
 import DashboardBox from "@/components/DashboardBox";
-import { useGetKpisQuery } from "@/state/api";
+// import { useGetKpisQuery } from "@/state/api";
 
 const Row1 = () => {
-  const { data } = useGetKpisQuery();
+  // const { data } = useGetKpisQuery();
   return (
     <>
       <DashboardBox gridArea="a"></DashboardBox>
