f7868940879ffebc1146b9a0c9d41f9f495d499f
Nayoung Kim <nka50@sfu.ca>
2025-11-03 16:45:56 -0800

    implement Operational vs Non-Operational Expenses LineChart with data visualization in Row2

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,9 +1,102 @@
+import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from "@/state/api";
+import { useTheme } from "@mui/material";
+import { useMemo } from "react";
+import {
+  ResponsiveContainer,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+} from "recharts";
 
 const Row2 = () => {
+  const { palette } = useTheme();
+  const { data: operationalData } = useGetKpisQuery();
+
+  const operationalExpenses = useMemo(() => {
+    return (
+      operationalData &&
+      operationalData[0].monthlyData.map(
+        ({ month, operationalExpenses, nonOperationalExpenses }) => {
+          return {
+            name: month.substring(0, 3),
+            "Operational Expenses": operationalExpenses,
+            "Non Operational Expenses": nonOperationalExpenses,
+          };
+        }
+      )
+    );
+  }, [operationalData]);
+
   return (
     <>
-      <DashboardBox gridArea="d"></DashboardBox>
+      <DashboardBox gridArea="d">
+        <BoxHeader
+          title="Operational vs Non-Operational Expenses"
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={operationalExpenses}
+            margin={{
+              top: 20,
+              right: 0,
+              left: -10,
+              bottom: 55,
+            }}
+          >
+            <CartesianGrid
+              yAxisId="left"
+              vertical={false}
+              horizontal={true}
+              stroke={palette.grey[800]}
+            />
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              yAxisId="left"
+              orientation="left"
+              tickLine={false}
+              axisLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              tickLine={false}
+              axisLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <Tooltip />
+            <Legend
+              height={20}
+              wrapperStyle={{
+                margin: "0 0 10px 0",
+              }}
+            />
+            <Line
+              yAxisId="left"
+              type="monotone"
+              dataKey="Non Operational Expenses"
+              stroke={palette.tertiary[500]}
+            />
+            <Line
+              yAxisId="right"
+              type="monotone"
+              dataKey="Operational Expenses"
+              stroke={palette.primary.main}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </DashboardBox>
       <DashboardBox gridArea="e"></DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
